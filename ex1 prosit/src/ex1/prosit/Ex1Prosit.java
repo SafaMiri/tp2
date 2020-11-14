5cc0e9a5f353717cd5da40f1be80169adc1f6a13
SafaMiri <mirisafa1@gmail.com>
2020-11-14 14:18:53 +0100

    added a feature

---
+++ ex1 prosit/src/ex1/prosit/Ex1Prosit.java
@@ -0,0 +1,29 @@
+package ex1.prosit;
+import java.util.Arrays;
+import java.util.Scanner;
+public class Ex1Prosit {
+    private static Object sc;
+       public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       System.out.println("veullez saisir la taille du tableau");
+       int n=sc.nextInt();
+       int x;
+       for(int i=0;i<n;i++)
+       {
+           do{
+               System.out.println("Donnez t["+ i + "] : ");
+               x=sc.nextInt();
+                          
+           }while(x<0);
+          
+           
+       }
+            
+        
+        
+    }
+}
+      
+    
+    
+
